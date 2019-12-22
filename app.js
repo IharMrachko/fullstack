384cc4e3809a37bcbde035da4acd8620e77db330
IharMrachko <ihormrochko@gmail.com>
2019-12-23 01:36:25 +0300

    b

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('/client/dist/client'));
     app.get('*', (req, res) =>{
         res.sendFile(
-            path.resolve(
+            path.join(
             __dirname, 'client', 'dist', 'client', 'index.html'
             )
         )
